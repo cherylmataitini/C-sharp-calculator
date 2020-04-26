f3f4753d24f9f52b5369b06fdf79ad3cd4391381
Cheryl Mataitini <cherylmataitini@gmail.com>
2020-04-26 20:48:15 +0100

    Initial commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                InputConverter inputConverter = new InputConverter();
+                CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+                double firstNumber = inputConverter.ConvertInputToNum(Console.ReadLine());
+                double secondNumber = inputConverter.ConvertInputToNum(Console.ReadLine());
+                string operation = Console.ReadLine();
+
+                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                // Should log the exceptions
+                Console.WriteLine(ex.Message);
+
+            }
+
+            }   
+    }
+} 
